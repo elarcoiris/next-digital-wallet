95e31c69e31019bfdd110ffdb6489932ac441e5d
Prue Phillips <pruephillips@hotmail.com>
2026-02-21 22:15:15 +1000

    update esnext

---
+++ src/app/services/store-card.ts
@@ -0,0 +1,55 @@
+const getStoreCard = (walletType: string, userId: string, token: string) => {
+    const url = 'http://localhost:3000/store-card'
+
+    if (walletType === 'apple') {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.blob();
+        }).then(blob => {
+            const filename = 'wallet.pkpass'
+            const type = 'application/vnd.apple.pkpass'
+            const file = new File([blob], filename, { type: type })
+            const reader = new FileReader()
+            reader.onload = () => {
+                const bdata = btoa(reader.result as string)
+                const datauri = `data:${type};base64,${bdata}`;
+                window.location.href = datauri;
+            };
+            reader.readAsDataURL(file);
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+    else {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json();
+        }).then(jsonResponse => {
+            const jwt = jsonResponse.jwt;
+            if (!jwt) {
+                throw new Error('Could not generate jwt')
+            }
+            window.location.href = `https://pay.google.com/gp/v/save/${jwt}`;
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+}
+
+export default getStoreCard;
